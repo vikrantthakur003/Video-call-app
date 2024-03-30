80ad5a0b7a09570c49bbfe449785bfe98ba37eb7
Vinayak <sharmavinayak744@gmail.com>
2024-03-31 03:17:06 +0530

    final code

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,8 +3,12 @@ const { authenticateUser, generateJWT } = require('../middleware');
 const router = require('express').Router();
 
 router.route('/').post(authenticateUser, generateJWT, async (req, res) => {
-  console.log('hi i reached here yeeeeeeeeeeeee')
-  res.redirect(301,'http://localhost:4000/public/lobby.html')
+  const accessToken = req.accessToken
+  console.log('hi i reached here yeeeeeeeeeeeee', accessToken)
+  res.setHeader('Authorization', `Bearer ${accessToken}`);
+  
+  // Send a success response with an empty body
+  res.status(200).json({ accessToken });
 });
 
 module.exports = router;
