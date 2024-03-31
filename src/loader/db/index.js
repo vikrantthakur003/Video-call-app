470943edd64918decf724ae312e6c3071344d0fb
Vinayak <sharmavinayak744@gmail.com>
2024-03-31 18:59:02 +0530

    production v4

--- src/loader/db/index.js
+++ src/loader/db/index.js
@@ -5,8 +5,6 @@ const databaseLoader = async() =>{
 
     try {
         await mongoose.connect(String(process.env.DB_STRING), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         })
         console.log('***  DB Connected😀  ***');
         
