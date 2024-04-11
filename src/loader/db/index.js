ec1b6608ffe1a4cade565129c0694176b828665f
vikrant3353005 <vikrantrana.k@gmail.com>
2024-04-12 00:07:53 +0530

    [Added] Comments and Testing API with front-end

--- src/loader/db/index.js
+++ src/loader/db/index.js
@@ -1,20 +1,32 @@
 const mongoose = require('mongoose');
 
 mongoose.set('debug', false);
-const databaseLoader = async () => {
+// const databaseLoader = async () => {
 
-    try {
-        await mongoose.connect(String(process.env.DB_SERVER), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: 'VideoConference' // Specify the database name here
-        })
+//     try {
+//         await mongoose.connect(String(process.env.DB_SERVER), {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             dbName: 'VideoConference' // Specify the database name here
+//         })
+//         console.log('***  DB Connected😀  ***');
+
+//     } catch (error) {
+//         console.log(`error while connecting to db: ${error}`);
+//         throw new Error(error)
+//     }
+// }
+
+
+const mongo = process.env.DB_SERVER
+const connectDatabase = async () =>  {
+    try  {
+        const response = await mongoose.connect(String(mongo))
         console.log('***  DB Connected😀  ***');
-
-    } catch (error) {
-        console.log(`error while connecting to db: ${error}`);
-        throw new Error(error)
+    }  catch(err)  {
+        console.log(err);
     }
 }
 
-module.exports = databaseLoader;
+
+module.exports = connectDatabase;
